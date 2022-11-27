6b0d0efc4f6b0685d11a6023d07eacd520a59c3e
Wergiley <werslley.robson@gmail.com>
2022-11-27 12:52:07 -0300

    att

---
+++ aulas/aula04.js
@@ -0,0 +1,16 @@
+// Aula: Funções
+
+function qudrado(v) {
+   return v * v;
+}
+
+console.log(qudrado(5));
+
+const areaQuadrado = qudrado(10);
+console.log(areaQuadrado);
+
+console.log(areaQuadrado + areaQuadrado);
+
+// Exercício: Faça os exercícios da aula anterior com funções.
+
+// Código aqui!
