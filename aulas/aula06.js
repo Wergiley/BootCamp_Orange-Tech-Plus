796c770933b2017b48f4e5ec6e961ff468420583
Wergiley <werslley.robson@gmail.com>
2022-11-29 22:38:49 -0300

    att

--- aulas/aula06.js
+++ aulas/aula06.js
@@ -5,3 +5,11 @@ const pessoa = {nome: "Vitor", idade: 25};
 
 console.log(pessoa.nome);
 console.log(pessoa.idade);
+console.log(pessoa);
+
+// atribuindo 
+pessoa.altura = 1.71;
+
+// deletando um atributo
+delete pessoa.nome;
+console.log(pessoa);
