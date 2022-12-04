a81eda3ffb43ada114d2f2a08bad0ddc18c6ab53
Wergiley <werslley.robson@gmail.com>
2022-12-03 23:17:59 -0300

    att

---
+++ aulas/aula08.js
@@ -0,0 +1,7 @@
+// Aula: Arrays e estrutura de repetição.
+
+const alunos = ['João', 'Pedro', 'Lucas', 'Paula','Roberta'];
+
+console.log(alunos.pop()); // <- mostra o ultimo valor do vetor.
+alunos.push('Leo'); // <- atribui um novo valor no final do vetor ou sequencial. 
+console.log(alunos);
