2a36a37a9b86cdab53cb6064df4f295786a2a9ce
Wergiley <werslley.robson@gmail.com>
2022-12-10 01:53:35 -0300

    att

---
+++ pokedex/assets/js/main.js
@@ -0,0 +1,23 @@
+function converterTypesPokemon(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${type}</li>`)
+}
+
+function converterListPokemon(pokemon) {
+    return `<li class="pokemon">
+            <span class="numer">${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class ="types">
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+            </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(converterListPokemon).join('')
+    pokemonList.innerHTML = newHtml
+})
