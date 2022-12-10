be2eaa6abfc068b8569234749eeb7e1ea5e94a36
Wergiley <werslley.robson@gmail.com>
2022-12-10 08:59:09 -0300

    att

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -3,12 +3,12 @@ function converterTypesPokemon(pokemonTypes) {
 }
 
 function converterListPokemon(pokemon) {
-    return `<li class="pokemon">
+    return `<li class="pokemon${pokemon.type}">
             <span class="numer">${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class ="types">
-                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="${type}">${type}</li>`).join('')}
                 </ol>
                 <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
